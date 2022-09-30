cfcdbe176780a2bf8a5a32fdadab4ff6b75d4444
André Bergan <andre.bergan@solv.no>
2022-09-30 13:49:35 +0200

    loops added

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,7 @@ fn main() {
     }
 
     // expressions do not include ending semicolons
+    // numbers by themselves are also expressions
     // by adding a semicolon at the end of an expression, you turn it into a statement and it will not return a value
     // expressions - the expression here is { let x = 3; x + 1 }
     let u = {
@@ -43,6 +44,77 @@ fn main() {
     let f = five();
     let p1 = plus_one(f);
     println!("p1 is: {p1}");
+
+    // if statements:
+    // rust does not automatically convert non-Boolean types to Boolean. You must be explicit and always provide if with a Boolean as its condition
+    let number = 6;
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    // because if is an expression, we can use it on the right side of a let statement to assign the outcome to a variable
+    let condition = true;
+    let number_test = if condition { 5 } else { 6 };
+    println!("The value of number_test is: {number_test}");
+
+    // loops:
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {result}");
+
+    // nested loops:
+    let mut count = 0;
+    // giving the loop a label
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                // exiting the outer loop
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    // looping through an array
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    // while loop
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    // for loop
+    for element in a {
+        println!("the value is: {element}");
+    }
+    // .. in range -- .rev() is used to reverse the range
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
 
 // in function signatures you must declare the type of each parameter
