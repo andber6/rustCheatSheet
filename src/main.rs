4e98b3aaaf5b590d2216236eb69d52ceaa0ed202
André Bergan <andre.bergan@solv.no>
2022-10-04 15:14:15 +0200

    slicing string and arrays. + referance

--- src/main.rs
+++ src/main.rs
@@ -200,3 +200,56 @@ fn plus_one(x: i32) -> i32 {
 fn convert_to_celsius(f: f64) -> f64 {
     (f-32.0) * (5.0/9.0)
 }
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn second_word(s: &String) -> (usize, usize) {
+    // string slices
+    let s = String::from("hello world");
+
+    // hello is a reference to a portion of the String specified in the extra [..5] bit
+    let hello = &s[..5];
+    let world = &s[6..]
+
+    let len = s.len(); 
+
+    // these are equal:
+    let slice = &s[0..len];
+    let slice = &s[..];
+
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+
+    // slicing part of an array:
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
+}
